2275ce9c7f1b861efef7f8f3806ad7b31b7a415c
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-04-08 19:53:25 +0200

    design the footer

--- include/templates/footer.php
+++ include/templates/footer.php
@@ -1,7 +1,77 @@
 
 
     <footer>
-
+        <div class="container">
+            <h2 class="text-second-color text-capitalize text-dark">hotels group media</h2>
+            <div class="row ">
+                <div class="col-sm-6 col-md-4 col-lg-2">
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a class="nav-link disabled fs-5">Top Destinations</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Las Vegas</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in New York</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Chicago</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Orlando</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in San Diego</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Los Angeles</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Miami</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in London</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Dallas</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Manchester</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Austin</a></li>   
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2">
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a class="nav-link disabled fs-5">Top Regions</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Maldives</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Bermuda</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in USA</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Singapore</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Turks</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Dubai</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Hotels in Abu Dhabi</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2">
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a class="nav-link disabled fs-5">Support & FAQs</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">Your Bookings</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">FAQs</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">Contact Us</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">Review A Property</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2">
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a class="nav-link disabled fs-5">Media</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Facebook</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Instagram</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Twitter X</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Telegram</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2">
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a class="nav-link disabled fs-5">Policies</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Terms & Conditions</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Privacy</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Cookies</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2">
+                    <ul class="nav flex-column">
+                        <li class="nav-item"><a class="nav-link disabled fs-5">Other Information</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Careers</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Guides</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr class="my-4">
+            <div class="copyrigth mb-3 text-center">
+                &copy; 2024 Hotels.local is an X group company. All rights reserved
+            </div>
+        </div>
     </footer>
     <!-- Include jquery script -->
     <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
